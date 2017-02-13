bbce294713dbe66962147f0d48cb48ebd17a4be1
Fun Project <funproject@Gs-MacBook-Air.local>
2017-02-13 01:32:33 -0700

    Added twbs, examples of blaze

--- client/main.js
+++ client/main.js
@@ -8,6 +8,37 @@ Template.hello.onCreated(function helloOnCreated() {
   this.counter = new ReactiveVar(0);
 });
 
+
+
+
+
+
+
+
+//ADDED AS AN EXAMPLE OF HOW DATA GETS PASSED TO HTML FILE
+//Notice the "testvariable" part in the line below.  In the HTML file, 
+//we insert it by {{testvariable}}
+//In application, this would actually have code that asks the database
+//for information and then returns it
+Template.hello.testvariable = function() {
+	var example = "This is a test variable -- pretend we asked the DB for it"
+	return example;
+}
+
+//Same thing, just another example.  This is called by {{clientID}}
+Template.hello.clientID = function() {
+	var ID = 123456;
+	return ID;
+}
+//ADDED AS AN EXAMPLE OF HOW DATABASE DATA GETS PASSED TO HTML FILE
+
+
+
+
+
+
+
+
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
