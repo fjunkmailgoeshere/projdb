5f052a8726247b24bca9ffeb1e04b3370caa2483
Fun Project <funproject@Gs-MacBook-Air.local>
2017-02-14 01:44:46 -0700

    Added router stuff, also broke down into a total of six pages based on KT's email

--- client/main.js
+++ client/main.js
@@ -3,51 +3,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-
-
-
-
-
-
-
 //ADDED AS AN EXAMPLE OF HOW DATA GETS PASSED TO HTML FILE
 //Notice the "testvariable" part in the line below.  In the HTML file, 
 //we insert it by {{testvariable}}
 //In application, this would actually have code that asks the database
 //for information and then returns it
-Template.hello.testvariable = function() {
+Template.main.testvariable = function() {
 	var example = "This is a test variable -- pretend we asked the DB for it"
 	return example;
 }
 
 //Same thing, just another example.  This is called by {{clientID}}
-Template.hello.clientID = function() {
+Template.main.clientID = function() {
 	var ID = 123456;
 	return ID;
 }
 //ADDED AS AN EXAMPLE OF HOW DATABASE DATA GETS PASSED TO HTML FILE
 
 
-
-
-
-
-
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
---
+++ routes.js
@@ -0,0 +1,10 @@
+Router.route('/', {
+    template: 'main'
+});
+
+
+Router.route("/consults");
+Router.route("/admin");
+Router.route("/communityaccess");
+Router.route("/trainings");
+Router.route("/reportquery");
