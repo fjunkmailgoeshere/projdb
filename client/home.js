f74de3db7ec45cd5a09fefbf1161bf1e09ff0085
fjunkmailgoeshere <forejunkmailgoeshere@gmail.com>
2017-02-27 23:35:45 -0700

    More work on admin, also fixed size of datepicker

--- client/home.js
+++ client/home.js
@@ -3,20 +3,3 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.home.events({
-	'click .adminlink': function() {
-		Router.go("admin");
-	},
-	'click .consultlink': function() {
-		Router.go("consults");
-	},
-	'click .traininglink': function() {
-		Router.go("trainings");
-	},
-	'click .reportquerylink': function() {
-		Router.go("reportquery");
-	},
-	'click .communityaccesslink': function() {
-		Router.go("communityaccess");
-	}
-});
--- routes.js
+++ routes.js
@@ -11,15 +11,44 @@ Router.route("/trainings");
 Router.route("/reportquery");
 
 
+
+
+
+// Trainings will change just like admin changed below
 Router.route("/trainings_addnewsession");
 Router.route("/trainings_createnewtrainingmodule");
 
+//Admin pages
+Router.route('/admin/addnewuser', {
+	name: 'admin_addnewuser',
+	template: 'admin_addnewuser'
+});
 
-Router.route("/admin_addnewuser");
-Router.route("/admin_resetpassword");
-Router.route("/admin_downloaddata");
-Router.route("/admin_editpermissions");
-Router.route("/admin_backup");
+Router.route('/admin/edituser', {
+	name: 'admin_edituser',
+	template: 'admin_edituser'
+});
+
+Router.route("/admin/changepassword", {
+	name: 'admin_changepassword',
+	template: 'admin_changepassword'
+});
+
+
+Router.route("/admin/downloaddata", {
+	name: 'admin_downloaddata',
+	template: 'admin_downloaddata'
+});
+
+Router.route("/admin/editpermissions", {
+	name: 'admin_editpermissions',
+	template: 'admin_editpermissions'
+});
+
+Router.route("/admin/backupdata", {
+	name: 'admin_backupdata',
+	template: 'admin_backupdata'
+});
 
 //Router.route('home', {
 //    path: "/home",
