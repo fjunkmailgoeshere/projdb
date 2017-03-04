fbcfd9e737acf7138c9a02b9d59682cef3ed9e8c
fjunkmailgoeshere <forejunkmailgoeshere@gmail.com>
2017-03-03 23:55:06 -0700

    Added DB functionality for consults page

--- client/consults/consults.js
+++ client/consults/consults.js
@@ -2,3 +2,29 @@
 Template.consults.rendered=function() {
 	$('#my-datepicker').datepicker();
 }
+
+
+Consults = new Mongo.Collection('consults');
+
+Template.consults.events({
+	'submit form': function(event) {
+		event.preventDefault();
+
+		var data = {
+			date: event.target.thedate.value,
+			name: event.target.name.value,
+			entity: event.target.entity.value,
+			hearingstatus: event.target.hearingstatus.value,
+			methodofcontact: event.target.methodofcontact.value,
+			reasonforcontact: event.target.reasonforcontact.value,
+			age: event.target.age.value,
+			gender: event.target.gender.value,
+			persontakingcall: event.target.persontakingcall.value,
+			resultofcall: event.target.resultofcall.value,
+			notes: event.target.notes.value
+		};
+
+		console.log(data);
+		Consults.insert(data);
+	}
+});
