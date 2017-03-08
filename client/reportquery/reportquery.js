7f127cb1e09a4287c15f6445b98a979cf04a5f1e
fjunkmailgoeshere <forejunkmailgoeshere@gmail.com>
2017-03-07 21:13:33 -0700

    Did some work on DB. About to branch for bootstrap, so will take care of this again later.  Wanted to push now.

--- client/reportquery/reportquery.js
+++ client/reportquery/reportquery.js
@@ -1,1 +1,21 @@
 /* code here */
+
+
+Template.reportquery_querytable.helpers({
+    'data': function(){
+	var db_entry = Consults.findOne();
+	console.log(db_entry);
+        return db_entry;
+    }
+
+
+
+});
+
+
+Template.reportquery.events({
+	'submit form': function(event) {
+		event.preventDefault();
+		alert("Form submitted");
+	}
+});
