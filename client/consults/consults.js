e40c477a60328fd9d0a52f2f565ac582c796d9b3
Katie Cue <katiecue@Katies-iMac.local>
2017-03-09 00:53:05 -0700

    Extensive changes to consults.html and consults.js files. Errors in .js files. Created navbar.html

--- client/consults/consults.js
+++ client/consults/consults.js
@@ -1,50 +1,256 @@
-/* code here */
-Template.consults.rendered=function() {
-	$('#my-datepicker').datepicker();
-}
+Template.consults
 
+	$(function(){
+    	//ajax mocks
+    
+	$.mockjaxSettings.responseTime = 500; 
+    
+    $.mockjax({
+        url: '/post',
+        response: function(settings) {
+            log(settings, this);
+        }
+    });
 
-Template.consults.events({
-	'submit form': function(event) {
-		event.preventDefault();
+    $.mockjax({
+        url: '/error',
+        status: 400,
+        statusText: 'Bad Request',
+        response: function(settings) {
+            this.responseText = 'Please input correct value'; 
+            log(settings, this);
+        }        
+    });
+    
+    $.mockjax({
+        url: '/status',
+        status: 500,
+        response: function(settings) {
+            this.responseText = 'Internal Server Error';
+            log(settings, this);
+        }        
+    });
+  
+    $.mockjax({
+        url: '/groups',
+        response: function(settings) {
+            this.responseText = [ 
+             {value: 0, text: 'Guest'},
+             {value: 1, text: 'Service'},
+             {value: 2, text: 'Customer'},
+             {value: 3, text: 'Operator'},
+             {value: 4, text: 'Support'},
+             {value: 5, text: 'Admin'}
+           ];
+           log(settings, this);
+        }        
+    });
+    
+    function log(settings, response) {
+            var s = [], str;
+            s.push(settings.type.toUpperCase() + ' url = "' + settings.url + '"');
+            for(var a in settings.data) {
+                if(settings.data[a] && typeof settings.data[a] === 'object') {
+                    str = [];
+                    for(var j in settings.data[a]) {str.push(j+': "'+settings.data[a][j]+'"');}
+                    str = '{ '+str.join(', ')+' }';
+                } else {
+                    str = '"'+settings.data[a]+'"';
+                }
+                s.push(a + ' = ' + str);
+            }
+            s.push('RESPONSE: status = ' + response.status);
 
-		//Check the checkboxes for reasonforcontact
-		var reason = [];
-		
-		if (event.target.communicationaccess.checked == true) { 
-			reason.push(event.target.communicationaccess.value);
-		}
-		if (event.target.hearingassistivetechnology.checked == true) { 
-			reason.push(event.target.hearingassistivetechnology.value);
-		}
-		if (event.target.cart.checked == true) { 
-			reason.push(event.target.cart.value);
-		}
-		if (event.target.referral.checked == true) { 
-			reason.push(event.target.referral.value);
-		}
-		if (event.target.technologyassistance.checked == true) { 
-			reason.push(event.target.technologyassistance.value);
-		}
+            if(response.responseText) {
+                if($.isArray(response.responseText)) {
+                    s.push('[');
+                    $.each(response.responseText, function(i, v){
+                       s.push('{value: ' + v.value+', text: "'+v.text+'"}');
+                    }); 
+                    s.push(']');
+                } else {
+                   s.push($.trim(response.responseText));
+                }
+            }
+            s.push('--------------------------------------\n');
+            $('#console').val(s.join('\n') + $('#console').val());
+    }    
+  
+   //defaults
+   $.fn.editable.defaults.url = '/post'; 
 
-		console.log(reason)
+            var c = window.location.href.match(/c=inline/i) ? 'inline' : 'popup';
+            $.fn.editable.defaults.mode = c === 'inline' ? 'inline' : 'popup';
 
+                $('#c').val(c);
+    //enable / disable
+   $('#enable').click(function() {
+       $('#user .editable').editable('toggleDisabled');
+   });    
+    
+    //editables 
+    $('#username').editable({
+           url: '/post',
+           type: 'text',
+           pk: 1,
+           name: 'username',
+           title: 'Enter username'
+    });
+	
+	$('#my-datepicker').datepicker({
+	
+	});
+	
+    $('#entity').editable({
+        prepend: "not selected",
+        source: [
+            {value: 1, text: 'I - Individual'},
+            {value: 2, text: 'GE - Government'}
+			{value: 3, text: 'PE - Public'}
+			{value: 4, text: 'CO - Community'}
+			{value: 5, text: 'PRE - Private'}
+    });
+	
+    $('#methodofcontact').editable({
+        prepend: "not selected",
+        source: [
+            {value: 1, text: 'Email'},
+            {value: 2, text: 'VP/Phone'}
+			{value: 3, text: 'In-person'}
+			{value: 4, text: 'Other'}
 
-		var data = {
-			date: event.target.thedate.value,
-			name: event.target.name.value,
-			entity: event.target.entity.value,
-			hearingstatus: event.target.hearingstatus.value,
-			methodofcontact: event.target.methodofcontact.value,
-			reasonforcontact: reason,
-			age: event.target.age.value,
-			gender: event.target.gender.value,
-			persontakingcall: event.target.persontakingcall.value,
-			resultofcall: event.target.resultofcall.value,
-			notes: event.target.notes.value
-		};
+    });
+	
+    $('#reasonforcontact').editable({
+        prepend: "not selected",
+        source: [
+            {value: 1, text: 'Communication Access'},
+            {value: 2, text: 'Hearing Assistive Technology'}
+			{value: 3, text: 'CART'}
+			{value: 4, text: 'Technology Assistance'}
+			{value: 5, text: 'Referral'}
+			{value: 6, text: 'Other'}
 
-		console.log(data);
-		Consults.insert(data);
-	}
+    });
+	
+    $('#hearingstatus').editable({
+        prepend: "not selected",
+        source: [
+            {value: 1, text: 'Deaf'},
+            {value: 2, text: 'Hard of Hearing'}
+			{value: 3, text: 'DeafBlind'}
+			{value: 4, text: 'Late Deafened'}
+			{value: 5, text: 'Hearing'}
+			{value: 6, text: 'Declined to Answer'}
+    });
+	
+
+$('#persontakingcall').editable({
+    prepend: "not selected",
+    source: [
+        {value: 1, text: 'Timothy'},
+        {value: 2, text: 'Cliff'}
+		{value: 3, text: 'Ryan'}
+		{value: 4, text: 'JoAnne'}
+		{value: 5, text: 'Cheri'}
+		{value: 6, text: 'Mandy'}
+	});
+    
+    $('#firstname').editable({
+        validate: function(value) {
+           if($.trim(value) == '') return 'This field is required';
+        }
+    });
+    
+   $('#gender').editable({
+      prepend: "not selected",
+      source: [
+        	{value: 1, text: 'Male'},
+            {value: 2, text: 'Female'}
+			{value: 3, text: 'Transgender'}
+			{value: 4, text: 'Non-binary'}
+			{value: 5, text: 'Declined to Answer'}
+			
+        ],
+        display: function(value, sourceData) {
+             var colors = {"": "gray", 1: "green", 2: "blue"},
+                 elem = $.grep(sourceData, function(o){return o.value == value;});
+                 
+             if(elem.length) {    
+                 $(this).text(elem[0].text).css("color", colors[value]); 
+             } else {
+                 $(this).empty(); 
+             }
+        }   
+    });    
+    
+    $('#status').editable();   
+    
+    $('#group').editable({
+       showbuttons: false 
+    });   
+
+    $('#vacation').editable({
+        datepicker: {
+            todayBtn: 'linked'
+        } 
+    });  
+        
+    $('#dob').editable();
+          
+    $('#event').editable({
+        placement: 'right',
+        combodate: {
+            firstItem: 'name'
+        }
+    });                  
+    
+    $('#comments').editable({
+        showbuttons: 'bottom'
+    }); 
+    
+    $('#note').editable(); 
+    $('#pencil').click(function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $('#note').editable('toggle');
+   }); 
+   
+   $('#coloradocounties').editable({
+	   source: ["Adams", "Alamosa", "Arapahoe", "Archuleta", "Baca", "Bent", "Boulder", "City and County of Broomfield", "Chaffee", "Cheyenne", "Clear Creek", "Conejos", "Costilla", "Crowley", "Custer", "Delta", "City and County of Denver", "Dolores", "Douglas", "Eagle", "El Paso", "Elbert", "Fremont", "Garfield", "Gilpin", "Grand", "Gunnison", "Hinsdale", "Huerfano", "Jackson", "Jefferson", "Kiowa", "Kit Carson", "La Plata", "Lake", "Larimer", "Las Animas", "Lincoln", "Logan", "Mesa", "Mineral", "Moffat", "Montezuma", "Montrose", "Morgan", "Otero", "Ouray", "Park", "Phillips", "Pitkin", "Prowers", "Pueblo", "Rio Blanco", "Rio Grande", "Routt", "Saguache", "San Juan", "San Miguel", "Sedgwick", "Summit", "Teller", "Washington", "Weld", "Yuma"]
+	
+   }); 
+   
+   $('#coloradocities').editable({
+	   source: ["Aguilar", "Akron", "Alamosa", "Alma", "Antonito", "Arriba", "Arvada", "Aspen", "Ault", "Aurora", "Avon", "Basalt", "Bayfield", "Bennett", "Berthoud", "Bethune", "Black Hawk", "Blanca", "Blue River", "Bonanza", "Boone", "Boulder", "Bow Mar", "Branson", "Breckenridge", "Brighton", "Brookside", "Broomfield", "Bush", "Buena Vista", "Burlington", "Calhan", "Campo", "Canon City", "Carbondale", "Castle Pines", "Castle Rock", "Cedaredge", "Centennial", "Center", "Central City", "Cheraw", "Cherry Hills Village", "Cheyenne Wells", "Coal Creek", "Cokedale", "Collbran", "Colorado Springs", "Columbine Valley", "Commerce City", "Cortez", "Craig", "Crawford", "Creede", "Crested Butte", "Crestone", "Cripple Creek", "Crook", "Crowley"]
+	
+   });  
+   
+   
+   $('#reasonforcontact').editable({
+       pk: 1,
+       limit: 6,
+       source: [
+        {value: 1, text: 'communication access'},
+        {value: 2, text: 'hearing assistive technology'},
+        {value: 3, text: 'CART'},
+        {value: 4, text: 'technology assistance'},
+        {value: 5, text: 'referral'}
+		{value: 6, text: 'other'}
+       ]
+    }); 
+                   
+         
+   $('#user .editable').on('hidden', function(e, reason){
+        if(reason === 'save' || reason === 'nochange') {
+            var $next = $(this).closest('tr').next().find('.editable');
+            if($('#autoopen').is(':checked')) {
+                setTimeout(function() {
+                    $next.editable('show');
+                }, 300); 
+            } else {
+                $next.focus();
+            } 
+        }
+   });
 });
